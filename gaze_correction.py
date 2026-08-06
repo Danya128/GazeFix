00adafb7dc42518f6921b85b72a384b7661f223d
Danya128 <oleynukd@gmail.com>
2026-08-06 12:37:48 +0100

    Initial gaze correction pipeline

---
+++ gaze_correction.py
@@ -0,0 +1,10 @@
+class GazeCorrection:
+    
+    def process(self, frame):
+        
+        plain_frame = frame.copy()
+        
+        # Future processes
+        corrected_frame = plain_frame
+        
+        return corrected_frame
