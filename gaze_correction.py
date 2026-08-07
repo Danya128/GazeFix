619e76add3ca6cf3acd844394d7d59a2bc0a49da
Danya128 <oleynukd@gmail.com>
2026-08-07 14:09:50 +0100

    Initialize a MediaPipe face landmark

--- gaze_correction.py
+++ gaze_correction.py
@@ -1,4 +1,24 @@
+import mediapipe as mp
+
 class GazeCorrector:
+    
+    def __init__(self):
+        base_options = mp.tasks.BaseOptions(
+            model_asset_path="assets/models/face_landmarker.task"
+        )
+
+        options = mp.tasks.vision.FaceLandmarkerOptions(
+            base_options=base_options,
+            running_mode=mp.tasks.vision.RunningMode.VIDEO,
+            num_faces=1,
+            min_face_detection_confidence=0.5,
+            min_face_presence_confidence=0.5,
+            min_tracking_confidence=0.5,
+        )
+
+        self.detector = mp.tasks.vision.FaceLandmarker.create_from_options(
+            options
+        )
     
     def process(self, frame):
         
