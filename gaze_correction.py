b2837a3a90078f55f3686f85c63e640e41ec6385
Danya128 <oleynukd@gmail.com>
2026-08-09 23:47:32 +0100

    Detect face landmarks

--- gaze_correction.py
+++ gaze_correction.py
@@ -1,4 +1,6 @@
 import mediapipe as mp
+import cv2
+import time
 
 class GazeCorrector:
     
@@ -19,10 +21,27 @@ class GazeCorrector:
         self.detector = mp.tasks.vision.FaceLandmarker.create_from_options(
             options
         )
+        
+        
+        
+    def detect_landmarks(self, frame):
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        timestamp = int(time.monotonic() * 1000)
+        
+        result = self.detector.detect_for_video(mp_image, timestamp)
+        return result
+    
+    
     
     def process(self, frame):
         
         plain_frame = frame.copy()
+        
+        result = self.detect_landmarks(frame)
+        if result.face_landmarks:
+            print("Face detected")
         
         # Future processes
         corrected_frame = plain_frame
