71216a82c4af2e2123d1938fb6248198e40e9345
Danya128 <oleynukd@gmail.com>
2026-08-13 13:16:29 +0100

    Implement head position estimation function

--- gaze_correction.py
+++ gaze_correction.py
@@ -9,6 +9,7 @@ RIGHT_IRIS_INDICES = [473, 474, 475, 476, 477]
 
 class GazeCorrector:
     
+    # Define FaceLandmark detector
     def __init__(self):
         base_options = mp.tasks.BaseOptions(
             model_asset_path="assets/models/face_landmarker.task"
@@ -29,6 +30,7 @@ class GazeCorrector:
         
         
         
+    # Get Landmarks
     def detect_landmarks(self, frame):
         
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -40,6 +42,7 @@ class GazeCorrector:
     
     
     
+    # Get iris center
     def get_iris_center(self, frame, face_landmarks, iris_indices):
         
         height, width = frame.shape[:2]
@@ -66,6 +69,85 @@ class GazeCorrector:
     
     
     
+    # Head Position
+    def get_head_pos(self, frame, face_landmarks):
+        
+        height, width = frame.shape[:2]
+        
+        image_points = np.array([
+            (
+                face_landmarks[1].x * width,
+                face_landmarks[1].y * height
+            ),# nose
+            (
+                face_landmarks[152].x * width,
+                face_landmarks[152].y * height
+            ),# chin
+            (
+                face_landmarks[33].x * width,
+                face_landmarks[33].y * height
+            ),  # left eye corner
+            (
+                face_landmarks[263].x * width,
+                face_landmarks[263].y * height
+            ),  # right eye corner
+            (
+                face_landmarks[61].x * width,
+                face_landmarks[61].y * height
+            ),  # left mouth corner
+            (
+                face_landmarks[291].x * width,
+                face_landmarks[291].y * height
+            ),  # right mouth corner
+
+        ], dtype=np.float64)
+        
+        #Template of 3D face model
+        model_points = np.array([
+            (0.0, 0.0, 0.0),# nose
+            (0.0, -63.6, -12.5),# chin
+            (-43.3, 32.7, -26.0),# left eye
+            (43.3, 32.7, -26.0),# right eye
+            (-28.9, -28.9, -24.1),# left mouth
+            (28.9, -28.9, -24.1),# right mouth
+        ], dtype=np.float64)
+        
+        focal_length = width
+        
+        # 3D Matrix for 3D face template
+        # Camera Intrinsic Matrix
+        camera_matrix = np.array([
+            [focal_length, 0, width/2],
+            [0, focal_length, height/2],
+            [0, 0, 1]
+        ], dtype=np.float64)
+        
+        distortion_coefficients = np.zeros((4, 1))
+        
+        success, rotation_vector, translation_vector = cv2.solvePnP(
+            model_points,
+            image_points,
+            camera_matrix,
+            distortion_coefficients
+        )
+        
+        if not success:
+            return None
+        
+        rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
+        
+        angles, _, _, _, _, _ = cv2.RQDecomp3x3(
+            rotation_matrix
+        )
+        
+        pitch = angles[0]
+        yaw = angles[1]
+        roll = angles[2]
+
+        return yaw, pitch, roll
+    
+    
+    # Main Process
     def process(self, frame):
         
         plain_frame = frame.copy()
@@ -76,8 +158,19 @@ class GazeCorrector:
         
         face_landmarks = result.face_landmarks[0]
         
+        # Iris Position
         left_iris_center = self.get_iris_center(plain_frame, face_landmarks, LEFT_IRIS_INDICES)
         right_iris_center = self.get_iris_center(plain_frame, face_landmarks, RIGHT_IRIS_INDICES)
+        
+        # Head Position
+        head_pos = self.get_head_pos(plain_frame, face_landmarks)
+        if head_pos:
+            yaw, pitch, roll = head_pos
+            print(
+                f"Yaw: {yaw:.1f}, "
+                f"Pitch: {pitch:.1f}, "
+                f"Roll: {roll:.1f}"
+            )
         
         # Future processes
         corrected_frame = plain_frame
